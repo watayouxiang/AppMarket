8a30b456bf2b848ab8aadaf55ca666ad1219283f
watayouxiang <watayouxiang@qq.com>
2016-04-05 11:49:50 +0800

    故障号: 0000000000 修改人: watayouxiang 修改模块: Android 修改内容: 应用(AppFragment)界面构建完成 修改类型: 需求实现 关联影响分析: 无

--- app/src/main/java/com/example/admin/appmarket/fragment/AppFragment.java
+++ app/src/main/java/com/example/admin/appmarket/fragment/AppFragment.java
@@ -1,21 +1,56 @@
 package com.example.admin.appmarket.fragment;
 
 import android.view.View;
+import android.widget.ListView;
 
 import com.example.admin.appmarket.base.BaseFragment;
+import com.example.admin.appmarket.base.BaseHolder;
+import com.example.admin.appmarket.base.MyBaseAdapter;
+import com.example.admin.appmarket.entity.AppInfo;
+import com.example.admin.appmarket.holder.AppHolder;
+import com.example.admin.appmarket.protocol.AppProtocol;
+import com.example.admin.appmarket.util.UIUtils;
 import com.example.admin.appmarket.widget.LoadingPage;
+
+import java.util.List;
 
 /**
  * Created by admin on 2016/3/23.
  */
 public class AppFragment extends BaseFragment {
+
+    private List<AppInfo> mData;
+
     @Override
     public View onSuccessedView() {
-        return null;
+        ListView listView = new ListView(UIUtils.getContext());
+        listView.setAdapter(new MyAdapter(mData));
+        return listView;
     }
 
     @Override
     public LoadingPage.ResultState onLoad() {
-        return LoadingPage.ResultState.STATE_EMPTY;
+        AppProtocol appProtocol = new AppProtocol();
+        mData = appProtocol.getData(0);
+        return check(mData);
+    }
+
+    class MyAdapter extends MyBaseAdapter{
+
+        public MyAdapter(List list) {
+            super(list);
+        }
+
+        @Override
+        public List onLoadMore() {
+            AppProtocol appProtocol = new AppProtocol();
+            List<AppInfo> moreData = appProtocol.getData(getListSize());
+            return moreData;
+        }
+
+        @Override
+        public BaseHolder getHolder() {
+            return new AppHolder();
+        }
     }
 }
---
+++ app/src/main/java/com/example/admin/appmarket/protocol/AppProtocol.java
@@ -0,0 +1,52 @@
+package com.example.admin.appmarket.protocol;
+
+import com.example.admin.appmarket.base.BaseProtocol;
+import com.example.admin.appmarket.entity.AppInfo;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by admin on 2016/4/5.
+ */
+public class AppProtocol extends BaseProtocol<List<AppInfo>> {
+    @Override
+    public String getKey() {
+        return "app";
+    }
+
+    @Override
+    public String getParams() {
+        return "";
+    }
+
+    @Override
+    public List<AppInfo> parseJson(String result) {
+        try {
+            JSONArray jsonArray = new JSONArray(result);
+            List<AppInfo> appInfoList = new ArrayList<AppInfo>();
+            appInfoList.clear();
+            for(int i = 0;i<jsonArray.length();i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                AppInfo appInfo = new AppInfo();
+                appInfo.setDes(jsonObject.getString("des"));
+                appInfo.setDownloadUrl(jsonObject.getString("downloadUrl"));
+                appInfo.setIconUrl(jsonObject.getString("iconUrl"));
+                appInfo.setId(jsonObject.getInt("id"));
+                appInfo.setName(jsonObject.getString("name"));
+                appInfo.setPackageName(jsonObject.getString("packageName"));
+                appInfo.setSize(jsonObject.getInt("size"));
+                appInfo.setStars((float)jsonObject.getDouble("stars"));
+                appInfoList.add(appInfo);
+            }
+            return appInfoList;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
